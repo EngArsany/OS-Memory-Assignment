fb26daefa253135dba78509a762819179bb75a14
EngArsany <arsanyo2003@gmail.com>
2026-05-04 16:01:58 +0300

    Add manipulation methods to Hole

--- src/Hole.py
+++ src/Hole.py
@@ -3,3 +3,12 @@ from Segment import Segment
 class Hole(Segment):
     def __init__(self, name, starting_address, size):
         super().__init__(name, size, starting_address)
+    
+    def set_size(self, newSize):
+        self.size = newSize
+    
+    def shrink_by(self, size):
+        self.size -= size
+    
+    def grow_by(self, size):
+        self.size += size
