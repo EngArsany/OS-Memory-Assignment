52b63e2e7ea5a82b62482f253d51e20100bcf173
EngArsany <arsanyo2003@gmail.com>
2026-05-04 15:25:58 +0300

    Add get_process

--- src/SegmentOfProcess.py
+++ src/SegmentOfProcess.py
@@ -2,11 +2,14 @@ from Segment import Segment
 # import Process
 
 class SegmentOfProcess(Segment):
-    def __init__(self, type, starting_address, size, process):
-        super().__init__(type, size, starting_address)
+    def __init__(self, data_type, starting_address, size, process):
+        super().__init__(data_type, size, starting_address)
         self.process = process
     
     # Override
     def print_info(self):
         print(self.process.get_name())
         print(self.name, ": ", self.size)
+
+    def get_process(self):
+        return self.process
