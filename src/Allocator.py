21c92b3df279af0049b5bfd3e991de41595f4ec8
EngArsany <arsanyo2003@gmail.com>
2026-05-04 15:43:10 +0300

    Add allocate_segment_to_hole method

--- src/Allocator.py
+++ src/Allocator.py
@@ -1,12 +1,23 @@
 from abc import ABC, abstractmethod
 from Process import Process
 from Memory import Memory
+from Segment import Segment
+from Hole import Hole
 
 class Allocator(ABC):
     def __init__(self, name):
         self.name = name
     
     @abstractmethod
-    def allocate(self, process : Process, memory : Memory):
+    def choose_hole(self, process : Process, memory : Memory) -> Hole:
         pass
 
+    def allocate_segment_to_hole(self, segment : Segment, hole : Hole, memory_block : dict):
+        hole_starting_address = hole.get_starting_address()
+        segment_size = segment.get_size()
+
+        segment.set_starting_address(hole_starting_address)
+        # Add the segment to the memory block  
+
+        hole.set_starting_address(hole_starting_address + segment_size)
+        hole.shrink_by(segment_size)
